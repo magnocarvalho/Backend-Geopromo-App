600f2f5c5a9207efdb4b2591ce6e603200b7a059
Magno Carvalho <Magno Carvalho@DESKTOP-UR94LR0>
2017-07-11 18:30:03 -0300

    login facebook criando conforme documentação do proprio facebook.

---
+++ AppFront/dbConfig.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration
+$dbHost     = "localhost";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbName     = "geopromo";
+
+//Create connection and select DB
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($db->connect_error) {
+    die("Unable to connect database: " . $db->connect_error);
+}
+?>
+
